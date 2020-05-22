1c2aeef13d8b5d14b2fbe376153c4db4508dc741
NordMan10 <42795088+NordMan10@users.noreply.github.com>
2020-05-22 17:04:26 +0500

    Update LinkedList.h

--- LinkedList/LinkedList.h
+++ LinkedList/LinkedList.h
@@ -1,24 +1,7 @@
 #pragma once
-#include <cassert>  
 
-template<typename T>
 class LinkedList
 {
-private:
-	template<typename T>
-	class Node
-	{
-	public:
-		Node* NextNode;
-		T Data;
-		Node(T data)
-		{
-			this->Data = data;
-			this->NextNode = nullptr;
-		}
-	};
-	
-	Node<T>* Head;
 public:
 	int Count;
 	LinkedList()
@@ -26,39 +9,38 @@ public:
 		Count = 0;
 		Head = nullptr;
 	}
-	
-	T operator[] (const int value)
+
+	int operator[] (const int value)
 	{
-		assert(value >= 0 && value < this->Count);
-		Node<T>* result = this->Head;
+		Node* result = this->Head;
 		for (auto i = 0; i < value; i++)
 			if (result != nullptr)
 				result = result->NextNode;
 		return result->Data;
 	}
 
-	void Add(T data)
+	void Add(int data)
 	{
 		if (Head == nullptr)
 		{
-			this->Head = new Node<T>(data);
+			this->Head = new Node(data);
 			this->Head->NextNode = nullptr;
 		}
 		else
 		{
-			Node<T>* current = this->Head;
+			Node* current = this->Head;
 			while (current->NextNode != nullptr)
 				current = current->NextNode;
-			current->NextNode = new Node<T>(data);
+			current->NextNode = new Node(data);
 		}
 		this->Count++;
 	}
 	void RemoveDuplicates()
 	{
-		Node<T>* temp;
-		Node<T>* previous;
-		Node<T>* current;
-		Node<T>* tempPrev;
+		Node* temp;
+		Node* previous;
+		Node* current;
+		Node* tempPrev;
 		for (previous = this->Head; previous != nullptr; previous = previous->NextNode)
 		{
 			tempPrev = previous;
@@ -79,12 +61,27 @@ public:
 			}
 		}
 	}
-	T SearchFromEnd(int k)
+	int SearchFromEnd(int k)
 	{
-		Node<T>* result = this->Head;
+		Node* result = this->Head;
 		for (auto i = 0; i < this->Count - k; i++)
 			result = result->NextNode;
 		return result->Data;
 	}
-};
 
+private:
+	class Node
+	{
+	public:
+		Node* NextNode;
+		int Data;
+		Node(int data)
+		{
+			this->Data = data;
+			this->NextNode = nullptr;
+		}
+	};
+	
+	Node* Head;
+
+};
