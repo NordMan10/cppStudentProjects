0bc24c8e4f47e0a1ced4ed0db00acc137dda150d
NordMan10 <42795088+NordMan10@users.noreply.github.com>
2020-05-24 13:13:39 +0500

    Update Matrix3x3.cpp

--- MatrixProject/Matrix3x3.cpp
+++ MatrixProject/Matrix3x3.cpp
@@ -15,31 +15,33 @@ void Matrix3x3::setElement(const int i, const int j, const int value)
 void Matrix3x3::fillRandomElements(const int minVal, const int maxVal)
 {
 	srand(time(0));
-	for (auto i = 0; i < 3; i++)
-		for (auto j = 0; j < 3; j++)
+	for (auto i = 0; i < dimension; i++)
+		for (auto j = 0; j < dimension; j++)
 			matrix[i][j] = minVal + rand() % (maxVal - minVal + 1);
 }
 
 int Matrix3x3::sumPrincipalDiag() const
 {
 	int sum = 0;
-	for (auto i = 0; i < 3; i++)	
-		sum += matrix[i][i];
+	for (auto i = 0; i < dimension; i++)
+		for (auto j = 0; j < dimension; j++)
+			if (i == j)	
+				sum += matrix[i][j];
 	return sum;
 }
 
 int Matrix3x3::sumSecondaryDiag() const
 {
 	int sum = 0;
-	for (auto i = 0; i < 3; i++)
-		sum += matrix[i][3 - i - 1];
+	for (auto i = 0; i < dimension; i++)
+		sum += matrix[i][dimension - i - 1];
 	return sum;
 }
 int Matrix3x3::productPrincipalDiag() const
 {
 	int sum = 1;
-	for (auto i = 0; i < 3; i++)
-		for (auto j = 0; j < 3; j++)
+	for (auto i = 0; i < dimension; i++)
+		for (auto j = 0; j < dimension; j++)
 			if (i == j)
 				sum *= matrix[i][j];
 	return sum;
@@ -47,21 +49,21 @@ int Matrix3x3::productPrincipalDiag() const
 int Matrix3x3::productSecondaryDiag() const
 {
 	int sum = 1;
-	for (auto i = 0; i < 3; i++)
+	for (auto i = 0; i < dimension; i++)
 		sum *= matrix[i][3 - 1 - i];
 	return sum;
 }
 int Matrix3x3::sumRow(const int iRow) const
 {
 	int sum = 0;
-	for (auto j = 0; j < 3; j++)
+	for (auto j = 0; j < dimension; j++)
 		sum += matrix[iRow][j];
 	return sum;
 }
 int Matrix3x3::minColumn(const int iCol) const
 {
 	int sum = INT32_MAX;
-	for (auto i = 0; i < 3; i++)
+	for (auto i = 0; i < dimension; i++)
 		if (matrix[i][iCol] < sum)
 			sum = matrix[i][iCol];
 	return sum;
@@ -69,7 +71,7 @@ int Matrix3x3::minColumn(const int iCol) const
 int Matrix3x3::maxColumn(const int iCol) const
 {
 	int sum = INT32_MIN;
-	for (auto i = 0; i < 3; i++)
+	for (auto i = 0; i < dimension; i++)
 		if (matrix[i][iCol] > sum)
 			sum = matrix[i][iCol];
 	return sum;
