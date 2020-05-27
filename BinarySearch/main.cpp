4477ba9c6be706f73744ccbb134c5caa74fd8a81
NordMan10 <42795088+NordMan10@users.noreply.github.com>
2020-05-27 14:18:18 +0500

    Update main.cpp

--- BinarySearch/main.cpp
+++ BinarySearch/main.cpp
@@ -9,8 +9,10 @@ int main()
 	BinaryTree binaryTree;
 	int myArray[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 	int length = sizeof(myArray) / sizeof(myArray[0]);
-	binaryTree.CreateMinimalBST(myArray, 0, length - 1);
+	binaryTree.SetRoot(binaryTree.CreateMinimalBST(myArray, 0, length - 1));
 	binaryTree.Insert(10);
 	cout << "Search result: " << binaryTree.Search(10)->getValue() << endl;
+	cout << "Search result: " << binaryTree.Search(5)->getValue() << endl;
+	cout << "Search result: " << binaryTree.Search(2)->getValue() << endl;
 	return 0;
 }
