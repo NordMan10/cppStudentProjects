6c9b6521621477a6db73f9d2e580c60263f77b89
NordMan10 <42795088+NordMan10@users.noreply.github.com>
2020-05-30 16:38:48 +0500

    Update Matrix2D.cpp

--- MatrixBase/Matrix2D.cpp
+++ MatrixBase/Matrix2D.cpp
@@ -1,9 +1,8 @@
 #include "Matrix2D.h"
 
-Matrix2D::Matrix2D(unsigned int iSize) : MatrixBase(iSize)
+Matrix2D::Matrix2D() : MatrixBase(size)
 {
-	auto count = size() * size();
-	for (auto i = 0; i < count; i++)
+	for (auto i = 0; i < size * size; i++)
 	{
 		matrix[i] = i + 1;
 	}
@@ -11,10 +10,10 @@ Matrix2D::Matrix2D(unsigned int iSize) : MatrixBase(iSize)
 
 int Matrix2D::element(unsigned int i, unsigned int j) const
 {
-	return matrix[i * size() + j];
+	return matrix[i * size + j];
 }
 
 int& Matrix2D::element(unsigned int i, unsigned int j)
 {
-	return matrix[i * size() + j];
+	return matrix[i * size + j];
 }
