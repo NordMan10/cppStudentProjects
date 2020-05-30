19ba14ef62b627b8b11789b4f090900b71a457d3
NordMan10 <42795088+NordMan10@users.noreply.github.com>
2020-05-30 16:39:50 +0500

    Update Matrix3D.h

--- MatrixBase/Matrix3D.h
+++ MatrixBase/Matrix3D.h
@@ -5,8 +5,10 @@
 class Matrix3D : public MatrixBase
 {
 public:
-	Matrix3D(unsigned int iSize);
+	Matrix3D();
 	int element(unsigned int i, unsigned int j) const override;
 	int& element(unsigned int i, unsigned int j) override;
+private:
+	static const int size = 3;
+	int matrix[size * size];
 };
-
