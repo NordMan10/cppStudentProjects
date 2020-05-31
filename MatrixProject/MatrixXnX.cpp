689783f688097c76330b4f843bc85474687cc06f
NordMan10 <42795088+NordMan10@users.noreply.github.com>
2020-05-31 19:36:14 +0500

    Update MatrixXnX.cpp

--- MatrixProject/MatrixXnX.cpp
+++ MatrixProject/MatrixXnX.cpp
@@ -2,60 +2,52 @@
 #include <iostream>
 #include <time.h>
 
-MatrixXnX::MatrixXnX(const int iDim)
+MatrixXnX::MatrixXnX(const int iDim) : dimension(iDim)
 {
-	dimension = iDim;
 	matrix = new int [iDim * iDim];
 	for (auto i = 0; i < iDim * iDim; i++)
 		matrix[i] = 0;
 }
-
 int MatrixXnX::element(const int i, const int j) const
 {
 	return matrix[i * dimension + j];
 }
-
 void MatrixXnX::fillRandomElements(const int minVal, const int maxVal)
 {
 	srand(time(0));
 	for (auto i = 0; i < dimension * dimension; i++)
 		matrix[i] = minVal + rand() % (maxVal - minVal + 1);
 }
-
 void MatrixXnX::setElement(const int i, const int j, const int value)
 {
 	matrix[i * dimension + j] = value;
 }
-
 int MatrixXnX::sumPrincipalDiag() const
 {
 	int sum = 0;
 	for (auto i = 0; i < dimension; i++)
-		sum += matrix[i * dimension + i];
+		sum += element(i, i);
 	return sum;
 }
-
 int MatrixXnX::sumSecondaryDiag() const
 {
 	int sum = 0;
 	for (auto i = 0; i < dimension; i++)
-		sum += matrix[i * dimension + dimension - i - 1];
+		sum += element(i, dimension - i - 1);
 	return sum;
 }
-
 int MatrixXnX::productPrincipalDiag() const
 {
 	int product = 1;
 	for (auto i = 0; i < dimension; i++)
-		product *= matrix[i * dimension + i];
+		product *= element(i, i);
 	return product;
 }
-
 int MatrixXnX::productSecondaryDiag() const
 {
 	int product = 1;
 	for (auto i = 0; i < dimension; i++)
-		product *= matrix[i * dimension + dimension - i - 1];
+		product *= element(i, dimension - i - 1);
 	return product;
 }
 
@@ -63,13 +55,13 @@ int MatrixXnX::sumRow(const int iRow) const
 {
 	int sum = 0;
 	for (auto i = 0; i < dimension; i++)
-		sum += matrix[iRow * dimension + i];
+		sum += element(iRow, i);
 	return sum;
 }
 
 int MatrixXnX::minColumn(const int iCol) const
 {
-	int min = INT32_MAX;
+	int min = std::numeric_limits<int>::max();
 	for (auto i = iCol; i < dimension * dimension; i += dimension)
 		if (matrix[i] < min)
 			min = matrix[i];
@@ -78,7 +70,7 @@ int MatrixXnX::minColumn(const int iCol) const
 
 int MatrixXnX::maxColumn(const int iCol) const
 {
-	int max = INT32_MIN;
+	int max = std::numeric_limits<int>::min();
 	for (auto i = iCol; i < dimension * dimension; i += dimension)
 		if (matrix[i] > max)
 			max = matrix[i];
