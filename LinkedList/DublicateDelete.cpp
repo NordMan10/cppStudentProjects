4599087556adacdbd03dde24216ddde6f802ffa2
NordMan10 <42795088+NordMan10@users.noreply.github.com>
2020-05-31 20:56:27 +0500

    Update DublicateDelete.cpp

--- LinkedList/DublicateDelete.cpp
+++ LinkedList/DublicateDelete.cpp
@@ -10,17 +10,17 @@ int main()
     linkedList.Add(3);
     linkedList.Add(2);
     linkedList.Add(8);
-    linkedList.Add(4);
+    linkedList.Add(2);
     linkedList.Add(13);
-    linkedList.Add(1);
+    linkedList.Add(8);
 
-    for (auto i = 0; i < linkedList.Count; i++)
+    for (auto i = 0; i < linkedList.Count(); i++)
         cout << linkedList[i] << endl;
     cout << "Second from the end: " << endl << linkedList.SearchFromEnd(2) << endl;
     cout << endl;
     linkedList.RemoveDuplicates();
     cout << "Without dublicates:" << endl;
-    for (auto i = 0; i < linkedList.Count; i++)
+    for (auto i = 0; i < linkedList.Count(); i++)
         cout << linkedList[i] << endl;
     cout << "Third from the end: " << endl << linkedList.SearchFromEnd(3);
 	return 0;
